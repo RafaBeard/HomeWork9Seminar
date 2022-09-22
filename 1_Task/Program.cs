cd7fc44fbab3c8af29dfe3b4fc38116a915d5b43
TRI <rafael.tagirzyanov@yandex.ru>
2022-09-22 16:43:44 +0300

    Первая задача

---
+++ 1_Task/Program.cs
@@ -0,0 +1,18 @@
+﻿int m = InputInt("Введите число M: ");
+int n = InputInt("Введите число N: ");
+Console.WriteLine(NaturalNumber(m, n));
+
+int NaturalNumber(int m, int n)
+{
+    if (m == n)
+        return m;
+    else
+        Console.Write($"{NaturalNumber(m, n - 1)}, ");
+    return n;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
